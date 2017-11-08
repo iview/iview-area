51383fa4cad0bd0755b80d9647e9613950bea516
zhigang.li <zhigang.li@tendcloud.com>
2017-11-08 18:47:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'https://iview.github.io/iview-area/dist/',
+    publicPath: './dist/',
     filename: 'build.js'
   },
   module: {
